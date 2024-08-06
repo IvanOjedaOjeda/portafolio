eb45abe267f51dde6c851093ce51a0f6930d3c53
Ivan <152219975+IvanOjedaOjeda@users.noreply.github.com>
2024-08-06 16:26:53 -0400

    commit URL Proyectos listo

--- app/url_proyectos/page.tsx
+++ app/url_proyectos/page.tsx
@@ -16,61 +16,68 @@ export default function ProyectosPage() {
       <Link isBlock showAnchorIcon href="https://ivanojedaojeda.github.io/Proyecto_2/" color="primary">
         Aplicación CRUD
       </Link>
-      <Link isBlock showAnchorIcon href="#" color="secondary">
+      <Link isBlock showAnchorIcon href="https://ivanojedaojeda.github.io/Proyecto3/" color="secondary">
         Tablero de Datos
       </Link>
-      <Link isBlock showAnchorIcon href="#" color="success">
-        Success
+      <Link isBlock showAnchorIcon href="https://ivanojedaojeda.github.io/resto2.1/" color="success">
+        Restorante
       </Link>
-      <Link isBlock showAnchorIcon href="#" color="warning">
-        Warning
+      <Link isBlock showAnchorIcon href="https://client-proyecto-5.vercel.app/" color="warning">
+        Comercio Electrónico
       </Link>
-      <Link isBlock showAnchorIcon href="#" color="danger">
-        Danger
-      </Link>      
+          
     </div>
-    <div style={{
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '20px',
-  justifyContent: 'center',
-  alignItems: 'center'
-}}>
-  <Image
-    width={300}
-    alt="Proyecto1"
-    src="proyecto1.png"
-    style={{ gridColumn: '1 / 2', gridRow: '1 / 2' }}
-  />
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        gridGap: "20px",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image
+        width={300}
+        alt="Proyecto1"
+        src="proyecto1.png"
+        style={{ gridColumn: "1 / 2", gridRow: "1 / 2" }}
+      />
 
-  <Image
-    width={300}
-    alt="Ferreteria don Chuma"
-    src="proyecto_2_ferreteria.png"
-    style={{ gridColumn: '2 / 3', gridRow: '1 / 2' }}
-  />
+      <Image
+        width={300}
+        alt="Ferreteria don Chuma"
+        src="proyecto_2_ferreteria.png"
+        style={{ gridColumn: "2 / 3", gridRow: "1 / 2" }}
+      />
 
-  <Image
-    width={300}
-    alt="NextUI hero Image"
-    src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-    style={{ gridColumn: '3 / 4', gridRow: '1 / 2' }}
-  />
+      <Image
+        width={300}
+        alt="API Paises"
+        src="paises.png"
+        style={{ gridColumn: "3 / 4", gridRow: "1 / 2" }}
+      />
 
-  <Image
-    width={600}
-    alt="NextUI hero Image"
-    src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-    style={{ gridColumn: '1 / 3', gridRow: '2 / 3' }}
-  />
+      <Image
+        width={300}
+        alt="Restorante"
+        src="restorante_proy4.png"
+        style={{ gridColumn: "2 / 3", gridRow: "2 / 3" }}
+      />
 
-  <Image
-    width={300}
-    alt="NextUI hero Image"
-    src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-    style={{ gridColumn: '3 / 4', gridRow: '2 / 3' }}
-  />
-</div>
+      <Image
+        width={300}
+        alt="Comercio electronico"
+        src="memeart.png"
+        style={{ gridColumn: "3 / 4", gridRow: "2 / 3" }}
+      />
+
+<Image
+        width={300}
+        alt="Comercio electronico"
+        src="proy6.png"
+        style={{ gridColumn: "3 / 4", gridRow: "2 / 3" }}
+      />
+    </div>
    
     </>
   );
